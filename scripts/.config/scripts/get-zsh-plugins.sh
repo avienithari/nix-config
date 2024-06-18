d0e5f7d7aebcaaabb8d5b23f5197aa0c40c59f68
avienithari <125766902+avienithari@users.noreply.github.com>
2024-06-18 21:49:35 +0200

    chore

---
+++ scripts/.config/scripts/get-zsh-plugins.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+
+cd ~
+git clone https://github.com/zsh-users/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
