9a676c4d66920e2d52bf9146b1879ab35d85af6c
avienithari <125766902+avienithari@users.noreply.github.com>
2025-04-18 19:38:05 +0200

    feat(scripts): remove unused

--- scripts/.config/scripts/build-nvim.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt-get install ninja-build gettext cmake unzip curl build-essential git
-git clone https://github.com/neovim/neovim
-cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
-sudo make install
--- scripts/.config/scripts/get-zsh-plugins.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-
-cd ~
-git clone https://github.com/zsh-users/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
