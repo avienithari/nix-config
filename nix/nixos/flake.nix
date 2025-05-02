8b45a58c81b685077d1cb42360cbf84b11ee2724
avienithari <125766902+avienithari@users.noreply.github.com>
2025-05-02 18:40:20 +0200

    chore

--- nix/nixos/flake.nix
+++ nix/nixos/flake.nix
@@ -26,22 +26,7 @@
             username = "avien";
           };
 
-          modules = [
-            ./hosts/magnamalo
-            ./modules/adb.nix
-            ./modules/avien.nix
-            ./modules/common-packages.nix
-            ./modules/desktop-packages.nix
-            ./modules/dev-packages.nix
-            ./modules/gnupg.nix
-            ./modules/hyprland.nix
-            ./modules/keyd.nix
-            ./modules/maintenance.nix
-            ./modules/ssh.nix
-            ./modules/steam.nix
-            ./modules/syncthing.nix
-            ./modules/tailscale.nix
-          ];
+          modules = [ ./hosts/magnamalo ];
         };
       };
     };
--- nix/nixos/hosts/magnamalo/default.nix
+++ nix/nixos/hosts/magnamalo/default.nix
@@ -3,6 +3,19 @@
 {
   imports = [
     ./configuration.nix
+    ../../modules/adb.nix
+    ../../modules/avien.nix
+    ../../modules/common-packages.nix
+    ../../modules/desktop-packages.nix
+    ../../modules/dev-packages.nix
+    ../../modules/gnupg.nix
+    ../../modules/hyprland.nix
+    ../../modules/keyd.nix
+    ../../modules/maintenance.nix
+    ../../modules/ssh.nix
+    ../../modules/steam.nix
+    ../../modules/syncthing.nix
+    ../../modules/tailscale.nix
   ];
 
   networking.hostName = lib.mkForce "magnamalo";
