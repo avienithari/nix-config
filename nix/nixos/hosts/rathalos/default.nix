0cbba324ec1505257d13a0e4663e3ace666293a0
avienithari <125766902+avienithari@users.noreply.github.com>
2025-05-03 12:13:04 +0200

    feat(nix-darwin): moving lld to module

--- nix/nixos/hosts/rathalos/default.nix
+++ nix/nixos/hosts/rathalos/default.nix
@@ -4,6 +4,7 @@
   imports = [
     ../../modules/darwin/common-packages.nix
     ../../modules/darwin/homebrew.nix
+    ../../modules/darwin/lld.nix
     ../../modules/darwin/settings.nix
     ../../modules/darwin/steam.nix
   ];
