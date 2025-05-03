0b4da463d66f2535a71b5be7f369ee08770a8bd8
avienithari <125766902+avienithari@users.noreply.github.com>
2025-05-03 09:53:42 +0200

    chore

--- nix/nixos/hosts/rathian/configuration.nix
+++ nix/nixos/hosts/rathian/configuration.nix
@@ -66,7 +66,6 @@
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
     docker-compose
-    tailscale
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
