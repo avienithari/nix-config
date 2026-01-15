a548bc5c2dd401b28ada67754b10b8817309d7ba
avienithari <125766902+avienithari@users.noreply.github.com>
2026-01-15 13:30:50 +0100

    feat(nix): add fonts

--- nix/modules/fonts.nix
+++ nix/modules/fonts.nix
@@ -5,15 +5,29 @@
     packages = with pkgs; [
       nerd-fonts.jetbrains-mono
       ubuntu-classic
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
     ];
 
     fontconfig = {
       enable = true;
 
       defaultFonts = {
-        serif = [ "Ubuntu Regular" ];
-        sansSerif = [ "Ubuntu Regular" ];
-        monospace = [ "Ubuntu Mono Regular" ];
+        serif = [
+          "Ubuntu Regular"
+          "Noto Serif CJK JP"
+          "Noto Serif CJK SC"
+        ];
+        sansSerif = [
+          "Ubuntu Regular"
+          "Noto Sans CJK JP"
+          "Noto Sans CJK SC"
+        ];
+        monospace = [
+          "Ubuntu Mono Regular"
+          "Noto Sans Mono CJK JP"
+        ];
       };
     };
   };
