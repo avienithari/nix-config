e7908ecfaeb4e39bfa473ccdbe09834be64f207a
avienithari <125766902+avienithari@users.noreply.github.com>
2026-01-20 17:43:34 +0100

    fix(nvim): lsp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,83 +1,89 @@
 return {
   "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+
   dependencies = {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
     "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-nvim-lsp",
     "j-hui/fidget.nvim",
     "folke/neodev.nvim",
     "stevearc/conform.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
 
   config = function()
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
     require("fidget").setup({})
-    require("mason").setup({})
-    require("neodev").setup({
-      library = { plugins = { "nvim-dap-ui" }, types = true },
-    })
-    require("mason-lspconfig").setup({
-      ensure_installed = {
-        "clangd",
-        "lua_ls",
-        "gopls",
-        "golangci_lint_ls",
-        "pyright",
-        "rust_analyzer",
-        "ts_ls",
-        "vimls",
-        "zls",
-        "html",
-        "templ",
-        "cssls",
-        "bashls",
-        "dockerls",
-        "docker_compose_language_service",
-      },
-      handlers = {
-        function(server_name)
-          require("lspconfig")[server_name].setup({
-            capabilities = capabilities,
-          })
-        end,
+    require("neodev").setup({})
 
-        ["lua_ls"] = function()
-          local lspconfig = require("lspconfig")
-          lspconfig.lua_ls.setup({
-            settings = {
-              Lua = {
-                diagnostics = {
-                  globals = { "vim" },
-                },
-              },
-            },
-          })
-        end,
+    local servers = {
+      bashls = {},
+      clangd = {},
+      cssls = {},
+      docker_compose_language_service = {},
+      dockerls = {},
+      golangci_lint_ls = {},
+      gopls = {},
+      html = {},
+      nixd = {},
+      pyright = {},
+      rust_analyzer = {},
+      templ = {},
+      ts_ls = {},
+      vimls = {},
+      zls = {},
+    }
+
+    local on_attach = function(_, bufnr)
+      local opts = { buffer = bufnr }
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+      vim.keymap.set("n", "<leader>rf", vim.lsp.buf.references, opts)
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+      vim.keymap.set("n", "<leader>ws", vim.lsp.buf.workspace_symbol, opts)
+
+      vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+
+      vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+    end
+
+    for name, cfg in pairs(servers) do
+      vim.lsp.config(name, {
+        capabilities = capabilities,
+        on_attach = on_attach,
+        settings = cfg.settings,
+      })
+      vim.lsp.enable(name)
+    end
+
+    vim.lsp.config("lua_ls", {
+      capabilities = capabilities,
+      on_attach = on_attach,
+      settings = {
+        Lua = {
+          diagnostics = { globals = { "vim" } },
+          workspace = {
+            library = vim.api.nvim_get_runtime_file("", true),
+            checkThirdParty = false,
+          },
+          telemetry = { enable = false },
+        },
       },
     })
-    require("mason-tool-installer").setup({
-      ensure_installed = {
-        "golangci-lint",
-        "htmlhint",
-        "pylint",
-        "gofumpt",
-        "goimports",
-        "stylua",
-        "delve",
-      },
-    })
+    vim.lsp.enable("lua_ls")
+
     require("conform").setup({
       formatters_by_ft = {
         lua = { "stylua" },
         nix = { "nixpkgs_fmt" },
+        go = { "gofumpt", "goimports" },
       },
     })
+
     vim.api.nvim_create_autocmd("BufWritePre", {
       callback = function(args)
         require("conform").format({
