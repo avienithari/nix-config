fdfc82d8feb3b44d9280ce6807c15e717ffc73fb
Maciej Borkowski <mmborkowski11@gmail.com>
2026-03-27 01:33:21 +0100

    feat(nix): introducing lagombi

---
+++ nix/hosts/lagombi/default.nix
@@ -0,0 +1,25 @@
+{ lib, ... }:
+
+{
+  imports = [
+    ./configuration.nix
+    ../../modules/avien.nix
+    ../../modules/bluetooth.nix
+    ../../modules/common-packages.nix
+    ../../modules/desktop-packages.nix
+    ../../modules/dev-packages.nix
+    ../../modules/fonts.nix
+    ../../modules/gnome.nix
+    ../../modules/gnupg.nix
+    ../../modules/maintenance.nix
+    ../../modules/nvidia.nix
+    ../../modules/ssh.nix
+    ../../modules/syncthing.nix
+  ];
+
+  networking = {
+    hostName = lib.mkForce "lagombi";
+  };
+
+  avien.ssh.passwordAuthentication = true;
+}
