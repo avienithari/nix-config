7dbf08bd4d1c89496a4d87c06d3a68506c4c1a63
avienithari <125766902+avienithari@users.noreply.github.com>
2026-04-08 15:35:13 +0200

    feat(nvim): change rendermarkdown for markview

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -3,7 +3,6 @@ return {
   "inkarkat/vim-ReplaceWithRegister",
   "mbbill/undotree",
   "tpope/vim-fugitive",
-  'MeanderingProgrammer/render-markdown.nvim',
   "avienithari/scribe",
   { "shortcuts/no-neck-pain.nvim", version = "*" },
 }
