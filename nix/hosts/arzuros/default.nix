3771d533c5746745d3a0316ad11d45655c4c3b05
avienithari <125766902+avienithari@users.noreply.github.com>
2026-04-10 02:47:10 +0200

    chore

--- nix/hosts/arzuros/default.nix
+++ nix/hosts/arzuros/default.nix
@@ -3,10 +3,10 @@
 {
   imports = [
     ./configuration.nix
-    ../../modules/avien.nix
     ../../modules/common-packages.nix
     ../../modules/desktop-packages.nix
     ../../modules/dev-packages.nix
+    ../../modules/fonts.nix
     ../../modules/gnupg.nix
     ../../modules/hyprland.nix
     ../../modules/maintenance.nix
