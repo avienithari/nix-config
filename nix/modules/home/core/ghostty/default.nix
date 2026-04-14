afd8e0e8dad0b5e503fc5aa0ec2c546470276ad8
avienithari <125766902+avienithari@users.noreply.github.com>
2026-04-14 03:21:40 +0200

    feat(nix): moving ghostty to home-manager

---
+++ nix/modules/home/core/ghostty/default.nix
@@ -0,0 +1,17 @@
+{ ... }:
+
+{
+  programs.ghostty = {
+    enable = true;
+    settings = {
+      font-size = 14;
+      font-family = "JetBrains Mono Nerd Font";
+      theme = "Rose Pine";
+      cursor-style = "block";
+      cursor-style-blink = false;
+      shell-integration-features = "no-cursor";
+      mouse-hide-while-typing = true;
+      mouse-scroll-multiplier = 2;
+    };
+  };
+}
