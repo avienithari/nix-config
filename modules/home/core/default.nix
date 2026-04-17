7a8bcf5586a4b819310752d01404d76aa0f997ae
avienithari <125766902+avienithari@users.noreply.github.com>
2026-04-17 11:31:11 +0200

    chore

--- modules/home/core/default.nix
+++ modules/home/core/default.nix
@@ -1,10 +1,6 @@
 { ... }:
 
 {
-  home.username = "avien";
-  home.homeDirectory = "/home/avien";
-  home.stateVersion = "26.05";
-
   imports = [
     ./bash
     ./fastfetch
--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,6 +1,10 @@
 { lib, vars, ... }:
 
 {
+  home.username = "avien";
+  home.homeDirectory = "/home/avien";
+  home.stateVersion = "26.05";
+
   imports = [
     ./core
   ] ++ lib.optionals (vars.class != "server") [
