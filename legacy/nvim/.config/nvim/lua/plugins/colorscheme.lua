e57718f7b10da12bf92fca02fd4cb80d57272cd5
avienithari <125766902+avienithari@users.noreply.github.com>
2026-04-23 14:56:59 +0200

    feat(nvim): enable italics in markdown buffers

--- legacy/nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ legacy/nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -14,6 +14,25 @@ return {
         },
       })
       vim.cmd([[colorscheme rose-pine]])
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "markdown",
+        callback = function()
+          local hl_groups = {
+            "markdownItalic",
+            "@markup.italic",
+            "@markup.italic.markdown_inline",
+          }
+
+          for _, group in ipairs(hl_groups) do
+            local hl = vim.api.nvim_get_hl(0, { name = group })
+
+            hl.italic = true
+
+            vim.api.nvim_set_hl(0, group, hl)
+          end
+        end,
+      })
     end,
   },
 }
