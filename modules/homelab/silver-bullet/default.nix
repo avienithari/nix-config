cb92506fea30f0693f381f86db2e48b0dce6b938
avienithari <125766902+avienithari@users.noreply.github.com>
2026-05-07 00:35:47 +0200

    feat(homelab): adding creds to silver-bullet

--- modules/homelab/silver-bullet/default.nix
+++ modules/homelab/silver-bullet/default.nix
@@ -1,14 +1,23 @@
-{ secrets, ... }:
+{ config, secrets, ... }:
 
 let
+  silverbulletPath = config.age.secrets.silverbullet.path;
+
   private = import "${secrets}/private.nix";
   domain = private.acme.domain;
 in
 {
+  age.secrets."silverbullet" = {
+    file = "${secrets}/silverbullet.age";
+    owner = "silverbullet";
+    group = "silverbullet";
+  };
+
   services = {
     silverbullet = {
       enable = true;
       listenPort = 8083;
+      envFile = silverbulletPath;
     };
 
     caddy.virtualHosts."notes.${domain}" = {
