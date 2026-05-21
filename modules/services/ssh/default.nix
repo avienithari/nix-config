ed74e2ca2ac2c55f56cc6137e9143fb2cabf71e7
avienithari <125766902+avienithari@users.noreply.github.com>
2026-05-21 03:30:22 +0200

    feat(nix): updating ssh module

--- modules/services/ssh/default.nix
+++ modules/services/ssh/default.nix
@@ -1,5 +1,8 @@
-{ lib, username, ... }:
+{ lib, username, secrets, ... }:
 
+let
+  private = import "${secrets}/private.nix";
+in
 {
   services.openssh = {
     enable = true;
@@ -7,8 +10,12 @@
       PasswordAuthentication = lib.mkDefault false;
       PermitRootLogin = "no";
       AllowUsers = [ "${username}" ];
+      MaxAuthTries = 3;
     };
   };
 
   networking.firewall.allowedTCPPorts = [ 22 ];
+
+  users.users.${username}.openssh.authorizedKeys.keys =
+    private.users.${username}.sshKeys;
 }
