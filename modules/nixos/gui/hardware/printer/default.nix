6af014e71f4063ae4c2b9af98b7363111acfbc71
avienithari <125766902+avienithari@users.noreply.github.com>
2026-06-08 14:52:31 +0200

    feat(nix): do not add printer to servers

--- modules/nixos/gui/hardware/printer/default.nix
+++ modules/nixos/gui/hardware/printer/default.nix
@@ -1,13 +1,15 @@
-{ ... }:
+{ config, lib, ... }:
 
 {
-  services = {
-    printing.enable = true;
+  config = lib.mkIf (config.host.class != "server") {
+    services = {
+      printing.enable = true;
 
-    avahi = {
-      enable = true;
-      nssmdns4 = true;
-      openFirewall = true;
+      avahi = {
+        enable = true;
+        nssmdns4 = true;
+        openFirewall = true;
+      };
     };
   };
 }
