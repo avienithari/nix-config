918ee17895c7145c829047a4c810d0f3bdd51ff5
avienithari <125766902+avienithari@users.noreply.github.com>
2026-06-17 14:53:01 +0200

    feat(nix): swapping stereo speakers on bazelgeuse

--- modules/nixos/gui/sound/default.nix
+++ modules/nixos/gui/sound/default.nix
@@ -17,7 +17,28 @@
 
         pulse.enable = true;
         jack.enable = true;
-        wireplumber.enable = true;
+        wireplumber = {
+          enable = true;
+
+          extraConfig."51-reverse-speakers" =
+            lib.mkIf (config.networking.hostName == "bazelgeuse") {
+              "monitor.alsa.rules" = [
+                {
+                  matches = [
+                    {
+                      "node.name" =
+                        "alsa_output.pci-0000_13_00.6.analog-stereo";
+                    }
+                  ];
+                  actions = {
+                    update-props = {
+                      "audio.position" = [ "FR" "FL" ];
+                    };
+                  };
+                }
+              ];
+            };
+        };
       };
     };
 
