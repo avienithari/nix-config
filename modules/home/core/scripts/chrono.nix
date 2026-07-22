48674f78886303773b437b5ec2861712a28f1f26
avienithari <125766902+avienithari@users.noreply.github.com>
2026-07-22 18:50:23 +0200

    feat(home-manager): adding alarm to chrono

--- modules/home/core/scripts/chrono.nix
+++ modules/home/core/scripts/chrono.nix
@@ -1,7 +1,9 @@
 { pkgs, ... }:
 
 let
-  bell = "${pkgs.sound-theme-freedesktop}/share/sounds/freedesktop/stereo/bell.oga";
+  theme = "${pkgs.sound-theme-freedesktop}/share/sounds/freedesktop/stereo";
+  bell = "${theme}/bell.oga";
+  alarm = "${theme}/alarm-clock-elapsed.oga";
 in
 pkgs.writeShellScriptBin "chrono" ''
   MODE=$1
@@ -15,8 +17,13 @@ pkgs.writeShellScriptBin "chrono" ''
       ${pkgs.termdown}/bin/termdown --no-art "$@" && \
         ${pkgs.sox}/bin/play -q ${bell} > /dev/null 2>&1
       ;;
+    alarm)
+      shift
+      ${pkgs.termdown}/bin/termdown --no-art "$@" && \
+        ${pkgs.sox}/bin/play -q ${alarm} trim 0 3 > /dev/null 2>&1
+      ;;
     *)
-      echo "Usage: chrono [stopwatch | timer <time>]"
+      echo "Usage: chrono [stopwatch | timer <time> | alarm <time>]"
       exit 1
       ;;
   esac
