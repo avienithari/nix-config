d3ec0c4839662498fbd7b6ace6dce83fdcfc172c
avienithari <125766902+avienithari@users.noreply.github.com>
2026-07-22 15:25:55 +0200

    feat(nix): adding sunshine to work work-packages

--- modules/nixos/misc/work-packages.nix
+++ modules/nixos/misc/work-packages.nix
@@ -1,10 +1,72 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, username, ... }:
 
+let
+  isWorkstation = config.host.isWorkstation;
+  isDesktop = config.host.class == "desktop";
+  isLaptop = config.host.class == "laptop";
+in
 {
-  config = lib.mkIf config.host.isWorkstation {
-    environment.systemPackages = with pkgs; [
-      brave
-      slack
-    ];
-  };
+  config = lib.mkMerge [
+    (lib.mkIf isWorkstation {
+      environment.systemPackages = with pkgs; [
+        brave
+        slack
+      ];
+    })
+
+    (lib.mkIf (isWorkstation && isLaptop) {
+      environment.systemPackages = with pkgs; [
+        moonlight-qt
+      ];
+    })
+
+    (lib.mkIf (isWorkstation && isDesktop) {
+      services.sunshine = {
+        enable = true;
+        autoStart = false;
+        capSysAdmin = true;
+        openFirewall = false;
+      };
+
+      boot = {
+        kernelModules = [ "uinput" ];
+        kernel.sysctl = {
+          "net.ipv4.ip_local_reserverd_ports" = "47984-48010";
+        };
+      };
+
+      hardware.graphics.enable = true;
+      users.users.${username}.extraGroups = [ "uinput" ];
+
+      networking.firewall.interfaces = {
+        "enp7s0" = {
+          allowedTCPPorts = [
+            47984
+            47989
+            48010
+          ];
+          allowedUDPPortRanges = [
+            {
+              from = 47998;
+              to = 48000;
+            }
+          ];
+        };
+
+        "tailscale0" = {
+          allowedTCPPorts = [
+            47984
+            47989
+            48010
+          ];
+          allowedUDPPortRanges = [
+            {
+              from = 47998;
+              to = 48000;
+            }
+          ];
+        };
+      };
+    })
+  ];
 }
