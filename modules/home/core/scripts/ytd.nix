551b61ef43cdb1bdb5170f7c55a3f4e4c2d0d7c2
avienithari <125766902+avienithari@users.noreply.github.com>
2026-08-03 02:07:44 +0200

    feat(home-manager): modernizing ytd

--- modules/home/core/scripts/ytd.nix
+++ modules/home/core/scripts/ytd.nix
@@ -1,18 +1,22 @@
 { pkgs, ... }:
 
+let
+  yt-dlp = "${pkgs.yt-dlp}/bin/yt-dlp";
+in
 pkgs.writeShellScriptBin "ytd" ''
   mode="$1"
   shift
 
   common_args=(
-    --output "~/Downloads/%(title)s.%(ext)s"
+    --paths "$HOME/Downloads"
+    --output "%(title)s.%(ext)s"
     --embed-metadata
     --embed-thumbnail
   )
 
   case "$mode" in
     audio)
-      exec ${pkgs.yt-dlp}/bin/yt-dlp \
+      exec ${yt-dlp} \
         --format "bestaudio" \
         -x \
         --audio-format flac \
@@ -20,15 +24,16 @@ pkgs.writeShellScriptBin "ytd" ''
         "$@"
       ;;
     video)
-      exec ${pkgs.yt-dlp}/bin/yt-dlp \
-        --format "bestvideo[ext=mp4][vcodec^=avc]+bestaudio[ext=m4a]/best[ext=mp4]/best" \
+      exec ${yt-dlp} \
+        --format \
+        "bestvideo[ext=mp4][vcodec^=avc]+bestaudio[ext=m4a]/best[ext=mp4]/best" \
         --merge-output-format mp4 \
         --remux-video mp4 \
         "''${common_args[@]}" \
         "$@"
       ;;
     *)
-      echo -e "Usage:\nytd audio <url>\nytd video <url>"
+      echo -e "Usage:\nytd audio <url> [-P <path>]\nytd video <url> [-P <path>]"
       exit 1
       ;;
   esac
