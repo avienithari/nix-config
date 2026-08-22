25243fae8977dcfcce8f0b76a80db5b07ee05d5b
avienithari <125766902+avienithari@users.noreply.github.com>
2026-08-22 05:33:57 +0200

    feat(installer-iso): disabling greeting messages

--- modules/installer/default.nix
+++ modules/installer/default.nix
@@ -1,4 +1,4 @@
-{ inputs, pkgs, modulesPath, username, ... }:
+{ lib, inputs, pkgs, modulesPath, username, ... }:
 
 let
   private = import "${inputs.secrets}/private.nix";
@@ -23,14 +23,21 @@ in
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  services.openssh = {
-    enable = true;
-    settings = {
-      PasswordAuthentication = false;
-      PermitRootLogin = "no";
-      LoginGraceTime = 0;
-      AllowUsers = [ "${username}" ];
-      MaxAuthTries = 3;
+  services = {
+    openssh = {
+      enable = true;
+      settings = {
+        PasswordAuthentication = false;
+        PermitRootLogin = "no";
+        LoginGraceTime = 0;
+        AllowUsers = [ "${username}" ];
+        MaxAuthTries = 3;
+      };
+    };
+
+    getty = {
+      helpLine = lib.mkForce "";
+      greetingLine = lib.mkForce "";
     };
   };
 
