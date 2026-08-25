6c4a14bc86b8826eb498ef9aff7c0ad2c89f1f67
avienithari <125766902+avienithari@users.noreply.github.com>
2026-08-25 21:08:10 +0200

    feat(home-manager): smol changes to fetch

--- modules/home/core/fastfetch/default.nix
+++ modules/home/core/fastfetch/default.nix
@@ -1,13 +1,45 @@
-{ ... }:
+{ lib, ... }:
 
+let
+  nixDarkBlue = "#5277C3";
+  nixLightBlue = "#7EBAE4";
+
+  logo = {
+    type = "builtin";
+    source = "nixos";
+    color = builtins.listToAttrs (
+      builtins.genList
+        (i: {
+          name = toString (i + 1);
+          value = if lib.mod i 2 == 0 then nixDarkBlue else nixLightBlue;
+        }) 6
+    );
+  };
+
+  ageModule = {
+    type = "command";
+    key = "Age";
+    keyColor = "36";
+    text = ''
+      birth=$(stat -c %W / 2>/dev/null)
+      if [ -z "$birth" ] || [ "$birth" -eq 0 ]; then
+        birth=$(stat -c %Y /)
+      fi
+
+      echo "$((($(date +%s) - birth) / 86400)) days"
+    '';
+  };
+in
 {
   programs.fastfetch = {
     enable = true;
     settings = {
+      inherit logo;
       modules = [
         "title"
         "separator"
         "os"
+        ageModule
         "host"
         "kernel"
         "uptime"
@@ -24,6 +56,7 @@
         "locale"
         "break"
         "colors"
+        "break"
       ];
     };
   };
